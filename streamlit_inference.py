cdaf5f426dba857a0ec4cdd248bec55033051b7c
K Ihe <226985594+ihe-k@users.noreply.github.com>
2025-09-05 21:13:32 +0100

    Update streamlit_inference.py

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -1037,7 +1037,7 @@ def run_app():
 
     #df_all = df_all.reset_index(drop=True)
 
-   html_table = df_all.head(50).to_html(index=False, classes="custom-table")
+    html_table = df_all.head(50).to_html(index=False, classes="custom-table")
     
 
     custom_css = """
