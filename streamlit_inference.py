0a4ede817a9c6216e286c4885e3a3b25cf4e981e
K Ihe <226985594+ihe-k@users.noreply.github.com>
2025-09-06 04:57:46 +0100

    Update streamlit_inference.py

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -1030,14 +1030,14 @@ def run_app():
         # Create a DataFrame
         #physio_df = pd.DataFrame(physio_data)
 
-         def classify_dementia_risk(breathing_rate, tapping_rate, heart_rate):
+         #def classify_dementia_risk(breathing_rate, tapping_rate, heart_rate):
             # Dummy logic for classification
-            if breathing_rate < 20 or tapping_rate < 5 or heart_rate < 100:
-                return "Dementia Risk: Low"
-            elif breathing_rate > 20 or tapping_rate > 3.0 or heart_rate > 100:
-                return "Dementia Risk: High"
-            else:
-                return "Dementia Risk: Medium"
+          #  if breathing_rate < 20 or tapping_rate < 5 or heart_rate < 100:
+          #      return "Dementia Risk: Low"
+          #  elif breathing_rate > 20 or tapping_rate > 3.0 or heart_rate > 100:
+          #      return "Dementia Risk: High"
+          #  else:
+           #     return "Dementia Risk: Medium"
         
         # Extracting the average values for prediction
         breathing_rate = physio_df["Breathing Rate"].mean()
