f1030273d8a0a445fdebf2a0e7ddcecd5192d7bf
K Ihe <226985594+ihe-k@users.noreply.github.com>
2025-09-06 16:45:36 +0100

    Update streamlit_inference.py

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -1013,7 +1013,7 @@ def run_app():
 
         # Display the table with the new Priority column
         st.write("**Audio Files Summary: Predicted Depression Severity Risk**")
-   #     st.dataframe(styled_df, use_container_width=True, hide_index=True)
+        st.dataframe(styled_df, use_container_width=True, hide_index=True)
 
         # Compute the MFCC mean value (from your features)
         mfcc_mean_value = np.mean(mfcc_features)  # or specific coefficient
@@ -1058,7 +1058,7 @@ def run_app():
        # st.info(f"Predicted Severity from MFCC Mean: {severity}")
 
         # Reset index for styling
-        summary_df_reset = summary_df.reset_index(drop=True)
+     #   summary_df_reset = summary_df.reset_index(drop=True)
 
         # Apply styling
        # styled_df = summary_df_reset.style.apply(apply_severity_color, axis=1)
