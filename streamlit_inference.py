cec8ef68e92972d2f38d0b3f0d8a7e4fc63701ad
K Ihe <226985594+ihe-k@users.noreply.github.com>
2025-09-10 18:33:16 +0100

    Update streamlit_inference.py

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -1569,7 +1569,7 @@ def run_app():
                     arts["explainer_shap"].expected_value,
                     shap_values_rounded[0],  
                     features=features_rounded[0],  
-                    feature_names=feature_labels,
+                    #feature_names=feature_labels,
                     matplotlib=True, 
                     show=False  
                 )
