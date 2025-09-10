5162f05654860822f46e7e99bbb7c85653046752
K Ihe <226985594+ihe-k@users.noreply.github.com>
2025-09-11 00:54:18 +0100

    Update streamlit_inference.py

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -1497,7 +1497,7 @@ def run_app():
                # Explanation block (LIME default like your sketch; SHAP optional)
             if method == "LIME":
                 st.subheader("LIME Explanation")
-                exclude_keywords = ['_std_', 'stf',"std"] 
+                exclude_keywords = ['_std_', 'stf',"std","_std_"] 
                 
                 keep_prefixes = ['PHQ']
                 
